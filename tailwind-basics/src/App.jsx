e5c5915773d42a28ee42d4428786546eddb5da6c
Sizzlesina <www.sins604@gmail.com>
2023-12-16 20:00:51 +0330

    States

--- tailwind-basics/src/App.jsx
+++ tailwind-basics/src/App.jsx
@@ -2,13 +2,16 @@
 
 function App() {
   return (
-    <div className="text-4xl">
-      <div className="invisible">base</div>
-      <div className="invisible sm:visible">sm</div>
-      <div className="invisible md:visible">md</div>
-      <div className="invisible lg:visible">lg</div>
-      <div className="invisible xl:visible">xl</div>
-      <div className="invisible 2xl:visible">2xl</div>
+    <div>
+      <button className="m-4 rounded-md bg-indigo-500 px-4 py-2 text-white transition-colors duration-300 hover:bg-indigo-600 focus:ring-2 focus:ring-green-500 active:bg-indigo-400">
+        Click me
+      </button>
+
+      <ul>
+        <li className="first:bg-green-400 last:bg-blue-400 odd:bg-purple-400 even:bg-yellow-300">First</li>
+        <li className="first:bg-green-400 last:bg-blue-400 odd:bg-purple-400 even:bg-yellow-300">Second</li>
+        <li className="first:bg-green-400 last:bg-blue-400 odd:bg-purple-400 even:bg-yellow-300">Third</li>
+      </ul>
     </div>
   );
 }
