4bb31e40b03b66374b025c00cc9c980a4117e06e
Sizzlesina <www.sins604@gmail.com>
2023-12-16 21:36:03 +0330

    Mobile nav

--- tailwind-project/src/components/Nav.jsx
+++ tailwind-project/src/components/Nav.jsx
@@ -1,8 +1,11 @@
 import NikeLogo from "../assets/nike-logo.svg?react";
 import { RxHamburgerMenu } from "react-icons/rx";
+
+const ROUTES = ["Home", "About", "Services", "Pricing", "Contact"];
+
 function Nav() {
   return (
-    <nav className="flex items-center justify-between ">
+    <nav className="flex flex-wrap items-center justify-between ">
       {/* Logo */}
       <a href="#">
         <NikeLogo className="h-20 w-20" />
@@ -12,6 +15,22 @@ function Nav() {
       <button className="rounded-lg p-2 hover:bg-gray-100 focus:ring-2 focus:ring-gray-200">
         <RxHamburgerMenu size={25} />
       </button>
+
+      {/* Menu list*/}
+      <div className="w-full ">
+        <ul className="rounded-lg border border-gray-100 bg-gray-50 p-4 text-lg">
+          {ROUTES.map((route, index) => (
+            <li
+              className={`cursor-pointer rounded px-3 py-2 ${
+                index === 0 ? "bg-blue-500 text-white" : "hover:bg-gray-100"
+              }`}
+              key={route}
+            >
+              {route}
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 }
