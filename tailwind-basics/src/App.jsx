889745db02f74497456e7c65c73e01510fe377d5
Sizzlesina <www.sins604@gmail.com>
2023-12-16 18:33:30 +0330

    Tailwind utilities

--- tailwind-basics/src/App.jsx
+++ tailwind-basics/src/App.jsx
@@ -1,7 +1,13 @@
 /** @format */
 
 function App() {
-  return <div className="">Hello world!</div>;
+  return (
+    <>
+      <div className='h-12 w-12 bg-green-500 '></div>
+      <div className='h-12 w-12 bg-red-500 '></div>
+      <div className='h-12 w-12 bg-blue-500 '></div>
+    </>
+  );
 }
 
 export default App;
