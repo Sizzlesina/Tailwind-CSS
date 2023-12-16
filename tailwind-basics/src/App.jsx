a3f398fe25883232411e6165d53873744c7f9821
Sizzlesina <www.sins604@gmail.com>
2023-12-16 19:23:52 +0330

    Flex and grids

--- tailwind-basics/src/App.jsx
+++ tailwind-basics/src/App.jsx
@@ -2,14 +2,20 @@
 
 function App() {
   return (
-    <>
-      <div className='h-12 w-12 bg-green-500 inline'>Hello</div>
-      <div className='block h-12 w-12 bg-red-500 '>My</div>
-      <div className='inline-block h-12 w-12 bg-red-500 '>Dear</div>
-      <div className='invisible h-12 w-12 bg-blue-500 '>Friend</div>
-      <div className='hidden h-12 w-12 bg-blue-500 '>Friend</div>
-      <div className='h-12 w-12 bg-blue-500 '>Friend</div>
-    </>
+    <div className=" grid h-screen grid-cols-6">
+      <div className="h-12 w-12 bg-green-500">Hello</div>
+      <div className="h-12 w-12 bg-red-500">My</div>
+      <div className="h-12 w-12 bg-blue-500">Friend</div>
+      <div className="h-12 w-12 bg-green-500">Hello</div>
+      <div className="h-12 w-12 bg-red-500">My</div>
+      <div className="h-12 w-12 bg-blue-500">Friend</div>
+      <div className="h-12 w-12 bg-green-500">Hello</div>
+      <div className="h-12 w-12 bg-red-500">My</div>
+      <div className="h-12 w-12 bg-blue-500">Friend</div>
+      <div className="h-12 w-12 bg-green-500">Hello</div>
+      <div className="h-12 w-12 bg-red-500">My</div>
+      <div className="h-12 w-12 bg-blue-500">Friend</div>
+    </div>
   );
 }
 
