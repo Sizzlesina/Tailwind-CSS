5bb0bac4c29eb16282273c8f6b1f32612a109ccd
Sizzlesina <www.sins604@gmail.com>
2023-12-16 20:46:01 +0330

    DarkMode

--- tailwind-basics/src/App.jsx
+++ tailwind-basics/src/App.jsx
@@ -2,22 +2,22 @@
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to Our Website</h1>
+    <div className="flex h-screen items-center justify-center">
+      <div className="rounded bg-white p-6 shadow-lg dark:bg-indigo-950 dark:shadow-sm">
+        <h2 className="text-2xl font-semibold text-gray-800 dark:text-white ">Card Title</h2>
+        <p className="mt-2 text-gray-600 dark:text-gray-400">
+          This is the main content of the card.
+        </p>
+        <p className="mt-2 text-gray-400">
+          Subtext or additional information goes here.
+        </p>
 
-      <h2>Tailwind is pretty dope</h2>
-      <p>Here : a lot of reasons why tailwind is dope</p>
-
-      <h2>React is pretty dope</h2>
-      <p>Here : a lot of reasons why React is dope</p>
-
-      <h2>NodeJS is pretty dope</h2>
-      <p>Here : a lot of reasons why NodeJS is dope</p>
-      <button className="btn-danger">Delete</button>
-      <button className="btn-danger">Danger</button>
-
-      <div className="flex-center h-44 bg-blue-200">
-        <p>Hi</p>
+        <button
+          onClick={() => document.documentElement.classList.toggle("dark")}
+          className="mt-4 rounded bg-indigo-500 px-4 py-2 font-semibold text-white hover:bg-indigo-600"
+        >
+          Button
+        </button>
       </div>
     </div>
   );
--- tailwind-basics/tailwind.config.js
+++ tailwind-basics/tailwind.config.js
@@ -6,6 +6,7 @@
 // eslint-disable-next-line
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
