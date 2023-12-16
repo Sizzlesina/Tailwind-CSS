c36ea9c6c87c913ddc0268dc44b003e51bd851a5
Sizzlesina <www.sins604@gmail.com>
2023-12-16 19:24:15 +0330

    Notes

--- notes/utilities.js
+++ notes/utilities.js
@@ -13,7 +13,13 @@
 ++ Invisible : it will not show the element but the place of the element still exists
 
 ++ Hidden : it will completely remove the element
-
 @ 5- flex 
 @ grid
+
+* Flex:
+@ - when we want to use the flex display we should have an container and then set the "flex" class for that container to behave as as a flex container
+
+@ - flex-col is used to positioning flex items vertically and flex-row is used for positioning flex items horizontally
+
+@ As simple as the name justify-(utility) is used for justify content of the flex items and items-(utility) is used for align items of the flex items
 */
