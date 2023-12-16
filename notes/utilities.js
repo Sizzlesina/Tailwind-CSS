6595c99b25bf1e834cd6a6aa9c1e46cbf6954dc7
Sizzlesina <www.sins604@gmail.com>
2023-12-16 18:43:47 +0330

    The display property

---
+++ notes/utilities.js
@@ -0,0 +1,19 @@
+/*
+* Display:
+@ 1- block => You can still change the height and width of the element:
+++ Example: div - h1 - p - li - section
+
+@ 2- inline => There going to be displayed on the same line and you cant change the height and width of it:
+++ Example: a - span - img - em - strong - i - small
+
+@ 3- inline-block => Its in the middle of block and inline and we can change the height and width of it
+
+@ 4- none => This display is not exist in Tailwind instead we have "invisible" and "hidden":
+
+++ Invisible : it will not show the element but the place of the element still exists
+
+++ Hidden : it will completely remove the element
+
+@ 5- flex 
+@ grid
+*/
