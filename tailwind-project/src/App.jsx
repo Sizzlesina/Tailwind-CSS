e3414dfaf1927b8b04f8b81041075ce5448d79ad
Sizzlesina <www.sins604@gmail.com>
2023-12-17 13:21:26 +0330

    Show detail layout

--- tailwind-project/src/App.jsx
+++ tailwind-project/src/App.jsx
@@ -1,11 +1,13 @@
 /** @format */
 
 import Nav from "./components/Nav";
+import ShoeDetai from "./components/ShoeDetail";
 
 function App() {
   return (
  <div className="p-10 xl:px-24 ">
   <Nav />
+  <ShoeDetai />
  </div>
   );
 }
