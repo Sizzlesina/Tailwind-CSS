ecbd0122071e66e5f4c5549b6f375e83c9782eb8
Sizzlesina <www.sins604@gmail.com>
2023-12-18 19:50:45 +0330

    Dark mode setup

--- tailwind-project/tailwind.config.js
+++ tailwind-project/tailwind.config.js
@@ -3,8 +3,16 @@
 // eslint-disable-next-line
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode : "class",
   theme: {
     extend: {
+      colors :{
+        night:{
+          DEFAULT : "#0D1120",
+          50 : "#171E2C",
+          500 : "#0D1120"
+        }
+      },
       keyframes: {
         wiggle: {
           "0%, 100%": { transform: "rotate(-3deg)" },
