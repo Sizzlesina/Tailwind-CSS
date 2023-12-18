bc618ad42f7f28f0a9de788b9661022929635b98
Sizzlesina <www.sins604@gmail.com>
2023-12-18 19:32:58 +0330

    Cart list

--- tailwind-project/src/App.jsx
+++ tailwind-project/src/App.jsx
@@ -7,7 +7,15 @@ import NewArrivalsSection from "./components/NewArrivalsSection";
 import ShoeDetai from "./components/ShoeDetail";
 import Sidebar from "./components/Sidebar";
 import { SHOE_LIST } from "./constant";
-import CartItem from "./components/CartItem";
+import Cart from "./components/Cart";
+
+const FAKE_CART_ITEMS = SHOE_LIST.map((shoe) => {
+  return {
+    product: shoe,
+    qty: 1,
+    size: 44,
+  };
+});
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -20,10 +28,7 @@ function App() {
         isOpen={isSidebarOpen}
         onClickClose={() => setIsSidebarOpen(false)}
       >
-        <h2 className="mb-10 text-2xl font-bold">Cart</h2>
-        <CartItem item={SHOE_LIST[0]} />
-        <CartItem item={SHOE_LIST[1]} />
-        <CartItem item={SHOE_LIST[2]} />
+        <Cart cartItems={FAKE_CART_ITEMS}/>
       </Sidebar>
     </div>
   );
--- tailwind-project/src/components/CartItem.jsx
+++ tailwind-project/src/components/CartItem.jsx
@@ -1,31 +1,41 @@
 import { CiTrash } from "react-icons/ci";
 import Select from "./Select";
 import { QTY, SIZES } from "../constant";
-function CartItem({ item }) {
+function CartItem({ item: { product, qty, size } }) {
   return (
     <div className="cursor-pointer space-y-2 bg-gray-50 p-2 hover:bg-[#DAFFA2] ">
       <div className="flex space-x-2">
         {/* Image */}
-        <img className="h-24" src={item.src} alt={item.title} />
+        <img className="h-24" src={product.src} alt={product.title} />
         <div className="space-y-2">
           {/* Title and Descriptions */}
-          <div className="font-bold">{item.title}</div>
-          <div className="text-sm text-gray-400">{item.description}</div>
+          <div className="font-bold">{product.title}</div>
+          <div className="text-sm text-gray-400">{product.description}</div>
         </div>
 
         {/* Price */}
-        <div className="font-bold">{item.price}$</div>
+        <div className="font-bold">{product.price}$</div>
       </div>
 
       <div className="flex justify-between pl-32 ">
         <div className="flex space-x-6 ">
           <div>
             <div className="font-bold">SIZE</div>
-            <Select title="" options={SIZES} className={"w-16 p-1"} />
+            <Select
+              defaultValue={size}
+              title=""
+              options={SIZES}
+              className={"w-16 p-1 pl-2"}
+            />
           </div>
           <div>
             <div className="font-bold">QTY</div>
-            <Select title="" options={QTY} className={"w-16 p-1"} />
+            <Select
+              defaultValue={qty}
+              title=""
+              options={QTY}
+              className={"w-16 p-1 pl-2"}
+            />
           </div>
         </div>
         <button>
